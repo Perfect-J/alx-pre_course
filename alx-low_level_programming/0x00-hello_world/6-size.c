51529d2bcd9765faa2acfd04e066797300c8846c
Perfec-J <johnsonayo532@gmail.com>
2022-09-08 15:30:43 -0700

    size

--- alx-low_level_programming/0x00-hello_world/6-size.c
+++ alx-low_level_programming/0x00-hello_world/6-size.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
-
-* main - print the string in the put function
-*
+* main -print the string in the put function 
 * Description: using the main function
-	this program prints "Programming is like building a multilingual puzzle
+	this program prints "programming is like building a multilingual puzzle
 	Return: 0
-
 */
 int main(void)
 {
@@ -18,9 +15,10 @@ int main(void)
 	float f;
 
 	printf("size of a char: %ld byte(s)\n", sizeof(0));
-	printf("size of a int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n" sizeof(li));
-	printf("size of a long long int: %ld byte(s)\n" sizeof(lli));
-	printf("size of a float: %ld byte(s)\n" sizeof(f));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
+
 }
